6b3bdde531997bab376ad8e96a0304c6e1789b3c
paultreanor <paul.treanor@fourtheorem.com>
2023-03-21 09:32:14 +0000

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+AWS_REGION=$(aws configure get region)
+S3_BUCKET_NAME="frontend-starter-${AWS_ACCOUNT_ID}-${AWS_REGION}"
+
+echo "AWS Account ID: ${AWS_ACCOUNT_ID}"
+echo "AWS Region: ${AWS_REGION}"
+echo "S3 Bucket Name: ${S3_BUCKET_NAME}"
+ 
+# Upload contents of /dist directory to S3 bucket
+aws s3 sync ./dist s3://${S3_BUCKET_NAME}
+
+# Invalidate CloudFront cache for updated files
+# aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths '/*'
