c44f0533665f90c23bb9b0b92fa4cf889f2a12b8
paultreanor <paul.treanor@fourtheorem.com>
2023-03-23 11:35:23 +0000

    Edit deploy.sh to empty S3 bucket before uploading

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,11 @@ echo "AWS Account ID: ${AWS_ACCOUNT_ID}"
 echo "AWS Region: ${AWS_REGION}"
 echo "S3 Bucket Name: ${S3_BUCKET_NAME}"
  
-# Upload contents of /dist directory to S3 bucket
-aws s3 sync ./dist s3://${S3_BUCKET_NAME}
+# Delete contents of S3 bucket (if it exists)
+aws s3 rm s3://${S3_BUCKET_NAME} --recursive
+
+# Upload contents of /frontend/dist directory to S3 bucket
+aws s3 sync ./frontend/dist s3://${S3_BUCKET_NAME}
 
 # Invalidate CloudFront cache for updated files
 # aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths '/*'
