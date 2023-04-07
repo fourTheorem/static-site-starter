e616dcfa47a701a4bd9934768700f3a9bd1aca67
paultreanor <paul.treanor@fourtheorem.com>
2023-04-07 10:49:28 +0100

    Remove deploy.sh line which deletes contents of S3 bucket

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,6 @@ S3_BUCKET_NAME="frontend-starter-${AWS_ACCOUNT_ID}-${AWS_REGION}"
 echo "AWS Account ID: ${AWS_ACCOUNT_ID}"
 echo "AWS Region: ${AWS_REGION}"
 echo "S3 Bucket Name: ${S3_BUCKET_NAME}"
- 
-# Delete contents of S3 bucket (if it exists)
-aws s3 rm s3://${S3_BUCKET_NAME} --recursive
 
 # Upload contents of /frontend/dist directory to S3 bucket
 aws s3 sync ./frontend/dist s3://${S3_BUCKET_NAME}
