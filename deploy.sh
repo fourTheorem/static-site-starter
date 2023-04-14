e0bedfb9a4d965e02ae5ca02c80c09bc4ac5dc55
paultreanor <paul.treanor@fourtheorem.com>
2023-04-14 11:12:27 +0100

    Deploy script deploys content of '/frontend'

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ echo "AWS Account ID: ${AWS_ACCOUNT_ID}"
 echo "AWS Region: ${AWS_REGION}"
 echo "S3 Bucket Name: ${S3_BUCKET_NAME}"
 
-# Upload contents of /frontend/dist directory to S3 bucket
-aws s3 sync ./frontend/dist s3://${S3_BUCKET_NAME}
+# Upload contents of /frontend directory to S3 bucket
+aws s3 sync ./frontend s3://${S3_BUCKET_NAME}
 
 # Invalidate CloudFront cache for updated files
 # aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths '/*'
