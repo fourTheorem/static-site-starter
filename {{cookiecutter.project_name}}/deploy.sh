b7233eb8c6acd1f4fbedc600d0c4097bb341d431
paultreanor <paul.treanor@fourtheorem.com>
2023-04-27 15:00:10 +0100

    Export values in deploy.sh

--- {{cookiecutter.project_name}}/deploy.sh
+++ {{cookiecutter.project_name}}/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-AWS_REGION=us-east-1
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
-S3_BUCKET_NAME="site-${AWS_ACCOUNT_ID}-${AWS_REGION}"
+export AWS_REGION=us-east-1
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+export S3_BUCKET_NAME="site-${AWS_ACCOUNT_ID}-${AWS_REGION}"
 
 echo "AWS Account ID: ${AWS_ACCOUNT_ID}"
 echo "AWS Region: ${AWS_REGION}"
